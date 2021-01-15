b70840f9da6f14a7605bb979fd3c8013b1a5b22b
eihwazzz <mike4hunter@hotmail.com>
2021-01-14 21:57:08 -0300

    Agregamos algunos modulos y los exportamos en SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  declarations: [],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     CommonModule
   ]
 })
